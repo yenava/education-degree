9b8da0062273740b3a19955af9aad8fea80d443f
Randal <545789177@qq.com>
2019-05-31 16:07:58 +0800

    finnalversion

---
+++ extension/highly_educated.js
@@ -0,0 +1,263 @@
+var myChart3 = echarts.init(document.getElementById('main3'));
+var option3 = {
+    baseOption:{
+        timeline:{
+            itemStyle:{
+                color:'#b9b0b0'
+            },
+            axisType: 'category',
+            symbol:'rect',
+            lineStyle:{
+                show:false,
+            },
+            autoPlay: true,
+            playInterval: 2000,
+            data:['2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017']
+        },
+        title: {
+            textColor:"#aa7f7b",
+            subtext: '数据来自国家统计局',
+            textStyle:{
+                fontSize:24
+            }
+        },
+        grid:{
+            x:100,
+            y:100
+        },
+        tooltip:{
+            backgroundColor:'#c75f65',
+            textColor:"#f2e9de",
+            borderColor:"#f2e9de",
+            borderWidth:3
+        },
+        legend:{
+            data: ['女', '男'],
+        },
+        yAxis: [
+            {
+                name:"学历",
+                'type':'category',
+                'axisLabel':{'interval':0},
+                'data':["普本专科","硕士"],
+                splitLine: {show: false},
+                axisLine: {
+                    lineStyle: {
+                        color: "#DC9FB4",
+                        width: 2,
+                    }
+                }
+            }
+        ],
+        xAxis: [
+            {
+                type: 'value',
+                name: '比例',
+                max: 60,
+                interval:30,
+                axisLabel:{
+                    show:true,
+                    formatter:'{value}%'
+                },
+                axisLine: {
+                    lineStyle: {
+                        color: "#DC9FB4",
+                        width: 2,
+                    }
+                },
+            }
+        ],
+        series: [
+            { // 系列一的一些其他配置
+                name:"女",
+                type: 'bar',
+                color:"#c75f65",
+                tooltip:{
+                    formatter:"{a}</br> {b}: {c}%"
+                },
+                markLine:{
+                    data:[{
+                        name:"中间线",
+                        xAxis:50
+                    }],
+                    symbol:"none"
+                },
+                label:{
+                    normal:{
+                        show:true,
+                        position:'right',
+                        fontWeight:'bold',
+                        formatter:function(params){
+                            return '{img1|}'
+                        },
+                        rich:{
+                            img1:{
+                                backgroundColor:{
+                                    image:'images/girl.png',
+                                },
+                                height:50,
+                                width:40
+                            }
+                        }
+                    }
+                }
+            },
+            { // 系列一的一些其他配置
+                name:"男",
+                type: 'bar',
+                color:"#f2e9de",
+                tooltip:{
+                    formatter:"{a}</br> {b}: {c}%"
+                },
+                label:{
+                    show:true,
+                    position:'right',
+                    fontWeight:'bold',
+                    formatter:function(params){
+                        return '{img1|}'
+                    },
+                    rich:{
+                        img1:{
+                            backgroundColor:{
+                                image:'images/boy.png',
+                            },
+                            height:50,
+                            width:40
+                        }
+                    }
+                }
+            },
+        ]
+    },
+    options: [
+        { // 这是'2002-01-01' 对应的 option
+            title: {
+                text: '2004年统计值'
+            },
+            series: [
+                {data:[dataPZ["女"][0],dataBS["女"][0]]},
+                {data:[dataPZ["男"][0],dataBS["男"][0]]},
+            ],
+
+        },
+        { // 这是'2002-01-01' 对应的 option
+            title: {
+                text: '2005年统计值'
+            },
+            series: [
+                {data:[dataPZ["女"][1],dataBS["女"][1]]},
+                {data:[dataPZ["男"][1],dataBS["男"][1]]},
+            ]
+        },
+        { // 这是'2002-01-01' 对应的 option
+            title: {
+                text: '2006年统计值'
+            },
+            series: [
+                {data: [dataPZ["女"][2],dataBS["女"][2]]},
+                {data: [dataPZ["男"][2],dataBS["男"][2]]},
+            ]
+        },
+        { // 这是'2002-01-01' 对应的 option
+            title: {
+                text: '2007年统计值'
+            },
+            series: [
+                {data: [dataPZ["女"][3],dataBS["女"][3]]},
+                {data: [dataPZ["男"][3],dataBS["男"][3]]},
+            ]
+        },
+        { // 这是'2002-01-01' 对应的 option
+            title: {
+                text: '2008年统计值'
+            },
+            series: [
+                {data: [dataPZ["女"][4],dataBS["女"][4]]},
+                {data: [dataPZ["男"][4],dataBS["男"][4]]},
+            ]
+        },
+        { // 这是'2002-01-01' 对应的 option
+            title: {
+                text: '2009年统计值'
+            },
+            series: [
+                {data: [dataPZ["女"][5],dataBS["女"][5]]},
+                {data: [dataPZ["男"][5],dataBS["男"][5]]},
+            ]
+        },
+        { // 这是'2002-01-01' 对应的 option
+            title: {
+                text: '2010年统计值'
+            },
+            series: [
+                {data: [dataPZ["女"][6],dataBS["女"][6]]},
+                {data: [dataPZ["男"][6],dataBS["男"][6]]},
+            ]
+        },
+        { // 这是'2002-01-01' 对应的 option
+            title: {
+                text: '2011年统计值'
+            },
+            series: [
+                {data: [dataPZ["女"][7],dataBS["女"][7]]},
+                {data: [dataPZ["男"][7],dataBS["男"][7]]},
+            ]
+        },
+        { // 这是'2002-01-01' 对应的 option
+            title: {
+                text: '2012年统计值'
+            },
+            series: [
+                {data: [dataPZ["女"][8],dataBS["女"][8]]},
+                {data: [dataPZ["男"][8],dataBS["男"][8]]},
+            ]
+        },
+        { // 这是'2002-01-01' 对应的 option
+            title: {
+                text: '2013年统计值'
+            },
+            series: [
+                {data: [dataPZ["女"][9],dataBS["女"][9]]},
+                {data: [dataPZ["男"][9],dataBS["男"][9]]},
+            ]
+        },
+        { // 这是'2002-01-01' 对应的 option
+            title: {
+                text: '2014年统计值'
+            },
+            series: [
+                {data: [dataPZ["女"][10],dataBS["女"][10]]},
+                {data: [dataPZ["男"][10],dataBS["男"][10]]},
+            ]
+        },
+        { // 这是'2002-01-01' 对应的 option
+            title: {
+                text: '2015年统计值'
+            },
+            series: [
+                {data: [dataPZ["女"][11],dataBS["女"][11]]},
+                {data: [dataPZ["男"][11],dataBS["男"][11]]},
+            ]
+        },
+        { // 这是'2002-01-01' 对应的 option
+            title: {
+                text: '2016年统计值'
+            },
+            series: [
+                {data: [dataPZ["女"][12],dataBS["女"][12]]},
+                {data: [dataPZ["男"][12],dataBS["男"][12]]},
+            ]
+        },
+        { // 这是'2002-01-01' 对应的 option
+            title: {
+                text: '2017年统计值'
+            },
+            series: [
+                {data: [dataPZ["女"][13],dataBS["女"][13]]},
+                {data: [dataPZ["男"][13],dataBS["男"][13]]},
+            ]
+        },
+
+    ]
+}
+myChart3.setOption(option3);
---
+++ extension/pile.js
@@ -0,0 +1,100 @@
+
+var myChart1 = echarts.init(document.getElementById('main1'));
+var option1 = {
+    tooltip : {
+        trigger: 'axis',
+        backgroundColor:'#c75f65',
+        formatter: function (params) {
+            let res = params[0].name + "年:" +'<br/>';
+            for(var i=params.length-1;i>=0;i--){
+                value = Math.floor(params[i].value*10000)/100+'%'
+                res +=params[i].seriesName+ value + '<br/>'
+            }
+            return res
+        },
+        textColor:"#f2e9de",
+        borderColor:"#f2e9de",
+        borderWidth:3,
+        axisPointer: {
+            type: 'cross',
+            label: {
+                backgroundColor: '#6a7985'
+            },
+        }
+    },
+    legend: {
+        data:['男','女']
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {}
+        },
+    },
+
+    xAxis : [
+        {
+            type : 'category',
+            boundaryGap : false,
+            axisLabel:{
+                interval:1
+            },
+            name:"年份",
+            axisLine:{
+                lineStyle:{
+                    color:"#DC9FB4",
+                    width:2,
+                }
+            },
+            label:{
+
+            },
+            data : ['2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017']
+        }
+    ],
+    yAxis : [
+        {
+            name:"百分比",
+            type : 'value',
+            splitLine:false,
+            axisLine:{
+                lineStyle:{
+                    color:"#DC9FB4",
+                    width:2,
+                }
+            },
+            axisLabel:{
+                formatter:function(params){
+                    return params*100 + "%"
+                }
+            },
+            axisTick:{
+                type:"dashed"
+            }
+
+        }
+    ],
+    series : [
+        {
+            name:'男',
+            type:'line',
+            symbol:"none",
+            smooth:true,
+            stack: '总量',
+            areaStyle: {},
+
+            data:[0.029469418, 0.028216608, 0.026558455, 0.028346009, 0.024029923, 0.021977766, 0.020739601, 0.020427378, 0.01822635, 0.016363291, 0.015987747, 0.015618746, 0.01600913, 0.016441854, 0.016955933, 0.015316454],
+            color:"#f2e9de",
+        },
+        {
+            name:'女',
+            type:'line',
+            symbol:"none",
+            smooth:true,
+            stack: '总量',
+            areaStyle: {},
+            data:[0.072784195, 0.068632866, 0.06507038, 0.075339794, 0.063858141, 0.058170114, 0.054263517, 0.050749724, 0.04963602, 0.038666988, 0.036933193, 0.034303266, 0.037715956, 0.040479404, 0.040081795, 0.037469988],
+            color:"#c75f65",
+        },
+    ]
+};
+myChart1.setOption(option1);
